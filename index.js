b8e8428e69e997e9aa12a8dd4996cc1840e65a21
sampada <55329464+sampada101@users.noreply.github.com>
2021-01-17 14:49:07 +0545

    Image slider with slideshow

---
+++ index.js
@@ -0,0 +1,74 @@
+var slideNo = 1;
+createDots();
+showSlides();
+
+function createDots(){
+  var images = document.getElementsByClassName("image");
+  for (var i = 0; i < images.length; i++) {
+    document.getElementById("DotsWrapper").innerHTML += `<span class="dot" onclick="showSlides(${i+1})"></span>`
+  }
+}
+
+function previousSlide() {
+  slideNo--
+  showSlides();
+}
+
+function nextSlide() {
+  slideNo++
+  showSlides();
+}
+
+function SlideshowTrue(){
+  if (!document.getElementById("annimation").checked){
+    document.getElementById("annimation").checked = true;
+    showSlides(slideNo+1);
+  }
+}
+
+function SlideshowFalse(){
+  if (document.getElementById("annimation").checked){
+    document.getElementById("annimation").checked = false;
+    showSlides();
+  }
+}
+
+function manageSlideNo(){
+    if (slideNo > document.getElementsByClassName("image").length) {
+      slideNo = 1
+    }    
+    if (slideNo < 1) {
+      slideNo = document.getElementsByClassName("image").length
+    }
+}
+
+function showSlides(n = slideNo) {
+  try{
+    clearTimeout(timeout)
+  }
+  catch(err){
+
+  }
+  console.log(slideNo)
+  slideNo = n
+  var images = document.getElementsByClassName("image");
+  var dots = document.getElementsByClassName("dot");
+  manageSlideNo()
+  for (i in dots) {
+      try {
+        images[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      catch(err) {
+      }
+  }
+  images[slideNo-1].style = "display: block; max-width: 100%"
+  dots[slideNo-1].className += " active";
+  try{
+      if (document.getElementById("annimation").checked){
+      slideshow = function(){nextSlide()}
+      timeout = setTimeout(slideshow, 3500);
+      }
+    }
+  catch(err){}
+}
